c9f90686443a97f6a711a70351d844aac31bd267
Fisher Coburn <fishercoburn@gmail.com>
2020-11-11 16:44:48 -0600

    Initial Commit

---
+++ Slots Demo/ContentView.swift
@@ -0,0 +1,108 @@
+//
+//  ContentView.swift
+//  Slots Demo
+//
+//  Created by Fisher Coburn on 6/17/20.
+//  Copyright © 2020 Fisher Coburn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State private var symbols = ["apple", "star", "cherry"]
+    private var betAmount = 5
+    @State private var numbers = [0, 0, 0]
+    @State private var credits = 1000
+    
+    var body: some View {
+        
+        ZStack{
+            //Creating the background of the app (first layer of the ZStack)
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/355))
+                .edgesIgnoringSafeArea(.all)
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            //Moving on to the top layer of the app
+            VStack {
+                Spacer()
+                //HStack for the top where it has the title and stars
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color(.yellow))
+                    
+                    Text("Swift UI Slots!")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Image(systemName: "star.fill")
+                    .foregroundColor(Color(.yellow))
+                }.scaleEffect(2)
+                Spacer()
+                //The credits counter
+                Text("Credits: " + String(credits))
+                    .foregroundColor(.black)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.5))
+                .cornerRadius(20)
+                
+                Spacer()
+                //Cards
+                HStack{
+                    Spacer()
+                    Image(symbols[numbers[0]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    Image(symbols[numbers[1]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    Image(symbols[numbers[2]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    Spacer()
+                }
+                Spacer()
+                //Button
+                Button(action: {
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count-1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count-1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count-1)
+                    
+                    if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2] {
+                        self.credits += self.betAmount * 10
+                    }
+                    else {
+                        self.credits -= self.betAmount
+                    }
+                }) {
+                    Text("Spin")
+                    .bold()
+                        .foregroundColor(.white)
+                        .padding(.all, 10)
+                        .padding([.leading, .trailing], 30)
+                        .background(Color.pink)
+                    .cornerRadius(20)
+                    
+                }
+                Spacer()
+            }
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
